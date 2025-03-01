44debc9f586f1b6693146ba43d5a6de764c10852
githubscientist <sathish.datascientist@gmail.com>
2025-03-01 13:06:49 +0530

    cors enabled

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const logger = require('./middlewares/logger');
 const cookieParser = require('cookie-parser');
 const errorRoute = require('./middlewares/errorRoute');
 const authRouter = require('./routes/authRoutes');
+const cors = require('cors');
 
 // create express app
 const app = express();
+
+// enable CORS
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 // parse the request body as JSON
 app.use(express.json());
